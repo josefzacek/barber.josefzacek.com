1aec98e04e10b026147a7f370dffe2a7041f711e
Josef Zacek <joezacek@gmail.com>
2022-08-23 17:53:38 +0100

    add contact form validation and functionto show Thank you message on submission

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+  function validateNextButton() {
+    var buttonDisabled = $('.get-in-touch-row form input[name="Name"]').val().trim() === '' ||
+      $('.get-in-touch-row form input[name="Email"]').val().trim() === '' ||
+      $('.get-in-touch-row form input[name="Phone"]').val().trim() === '' ||
+      $('.get-in-touch-row form input[name="Subject"]').val().trim() === '' ||
+      $('.get-in-touch-row form textarea').val().trim() === '';
+
+    if (buttonDisabled) {
+      $('.get-in-touch-row button').attr('disabled', 'disabled');
+    } else {
+      $('.get-in-touch-row button').removeAttr('disabled');
+    }
+  }
+
+  $('.get-in-touch-row form input').on('keyup', validateNextButton);
+  $('.get-in-touch-row form textarea').on('keyup', validateNextButton);
+
+  $(".get-in-touch-row button").click(function(e) {
+    e.preventDefault();
+    $(".get-in-touch-row form").addClass("d-none");
+    $(".form-submitted-text").removeClass("d-none");
+  })
+
+})
